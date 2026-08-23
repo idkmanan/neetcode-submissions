503f72c59a17f9487ba956191781ce5e150b0045
Manan Aggarwal <129099436+idkmanan@users.noreply.github.com>
2026-08-23 12:28:04 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int n=nums.size();
+        int s=0,e=n-1,res=0;
+        while(s<e){
+            int mid = s+(e-s)/2;
+            if(nums[mid] < nums[e]){
+                e = mid;
+            } else {
+                s = mid+1;
+            }
+        }
+        return nums[s];
+    }
+};
